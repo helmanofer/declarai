379c67fd804fc3e0991e3119eadf983e2b7ff27d
Ofer Helman <helmanofer@gmail.com>
2023-08-25 20:56:47 +0000

    removed multiple types commit
    Signed-off-by: Ofer Helman <helmanofer@gmail.com>

--- tests/python_parser/annotations/test_type_annotation_to_schema.py
+++ tests/python_parser/annotations/test_type_annotation_to_schema.py
@@ -20,7 +20,7 @@ class MockComplexModelArray(BaseModel):
 
 class MockComplexModelDict(BaseModel):
     name: str
-    id: str | int = Field(description="Describe this field")
+    id: str = Field(description="Describe this field")
     children: Dict[str, MockSimpleModel]
 
 
@@ -62,7 +62,7 @@ class MockComplexModelDict(BaseModel):
             MockComplexModelDict,
             "{{\n"
             '    "name": "string",\n'
-            '    "id": "string or integer - Describe this field",\n'
+            '    "id": "string - Describe this field",\n'
             '    "children": {{\n'
             '        "name": "string",\n'
             '        "numbers": [\n'
